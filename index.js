8b6931bfebf51fff3b27007cbe00ce7f698d8960
saipraveen <ossp.ctr@gmail.com>
2023-10-28 10:02:44 +0000

    feat: practice nodejs backend

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const retVal = {
+      "ipaddress": req.ip,
+      "language": req.headers["accept-language"],
+      "software": req.headers["user-agent"]
+      };
+  console.log(retVal);
+  res.json(retVal);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
